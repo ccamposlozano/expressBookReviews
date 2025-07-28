41e0c77304edb485a0c493e260106e857ea3aa90
ccamposlozano <carlos.campos95@outlook.com>
2025-07-28 15:37:55 -0400

    Implemented login

--- final_project/router/auth_users.js
+++ final_project/router/auth_users.js
@@ -14,15 +14,33 @@ const authenticatedUser = (username,password)=>{ //returns boolean
 }
 
 //only registered users can login
-regd_users.post("/login", (req,res) => {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
-});
+regd_users.post("/login", (req, res) => {
+    const username = req.body.username;
+    const password = req.body.password;
+  
+    if (!username || !password) {
+      return res.status(400).json({ message: "Username and password are required" });
+    }
+  
+    const user = users.find((u) => username === u.username && password === u.password);
+    if (!user) {
+      return res.status(401).json({ message: "Invalid username or password" });
+    }
+  
+    const accessToken = jwt.sign(
+      { username: user.username, role: "user" },
+      "access",
+      { expiresIn: "1h" }
+    );
+  
+    req.session.authorization = { accessToken, username: user.username };
+  
+    return res.status(200).json({ message: "User logged in successfully" });
+  });
 
 // Add a book review
 regd_users.put("/auth/review/:isbn", (req, res) => {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+    
 });
 
 module.exports.authenticated = regd_users;
